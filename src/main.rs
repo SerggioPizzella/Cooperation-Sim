3ae9418837efabe5253219269fece926617929d3
Pizzella Ricci, Serggio V <serggiopizzella@gmail.com>
2022-06-28 18:33:16 +0200

    maybe fixed, looks good

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, vec};
+use std::{cmp::Ordering, vec, thread::sleep, time::Duration};
 
 use rand::{
     distributions::{Distribution, Standard},
@@ -6,15 +6,17 @@ use rand::{
 };
 
 fn main() {
-    let width = 20;
-    let height = 20;
-    let mut playing_field = PlayingField::new(width, height);
+    let mut arguments = std::env::args().skip(1);
+    let size: usize = arguments.next().unwrap().parse().unwrap();
+    let iterations: usize = arguments.next().unwrap().parse().unwrap();
+    let mut playing_field = PlayingField::new(size, size);
 
-    let aplha = 10_f32;
+    let aplha = 1.3_f32;
 
-    for _ in 0..200 {
-        print_playing_field(&playing_field);
+    for _ in 0..iterations {
+        print_playing_field(&playing_field).expect("idk man");
         playing_field.step(aplha);
+        sleep(Duration::from_millis(500));
     }
 }
 
@@ -33,7 +35,7 @@ impl Distribution<Strategy> for Standard {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Patch {
     strategy: Strategy,
     score: f32,
@@ -80,15 +82,19 @@ impl PlayingField {
         }
     }
 
+    fn with_patches(patches: Vec<Vec<Patch>>) -> PlayingField {
+        PlayingField { width: patches.len(), height: patches[0].len(), patches }
+    }
+
     fn step(&mut self, alpha: f32) {
         let patches = self.patches.to_owned();
         let w = self.width as isize;
         let h = self.height as isize;
 
         // Calculate the score for all patches
-        let mut neighbours = Vec::with_capacity(8);
         for x in 0..self.patches.len() as isize{
             for y in 0..self.patches[x as usize].len() as isize{
+                let mut neighbours = Vec::with_capacity(8);
                 neighbours.push(&patches[((((x - 1) % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize]);
                 neighbours.push(&patches[((((x - 1) % w) + w) % w) as usize][(((y % h) + h) % h) as usize]);
                 neighbours.push(&patches[((((x - 1) % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize]);
@@ -107,22 +113,21 @@ impl PlayingField {
             for y in 0..self.patches[x as usize].len() as isize {
                 let winning_patch = {
                     let neighbours = [
-                        &patches[((((x - 1) % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
-                        &patches[((((x - 1) % w) + w) % w) as usize][(((y % h) + h) % h) as usize],
-                        &patches[((((x - 1) % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize],
-                        &patches[(((x % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
-                        &patches[(((x % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize],
-                        &patches[((((x + 1) % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
-                        &patches[((((x + 1) % w) + w) % w) as usize][(((y % h) + h) % h) as usize],
-                        &patches[((((x + 1) % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize]
+                        &self.patches[((((x - 1) % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
+                        &self.patches[((((x - 1) % w) + w) % w) as usize][(((y % h) + h) % h) as usize],
+                        &self.patches[((((x - 1) % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize],
+                        &self.patches[(((x % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
+                        &self.patches[(((x % w) + w) % w) as usize][(((y % h) + h) % h) as usize],
+                        &self.patches[(((x % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize],
+                        &self.patches[((((x + 1) % w) + w) % w) as usize][((((y - 1) % h) + h) % h) as usize],
+                        &self.patches[((((x + 1) % w) + w) % w) as usize][(((y % h) + h) % h) as usize],
+                        &self.patches[((((x + 1) % w) + w) % w) as usize][((((y + 1) % h) + h) % h) as usize]
                     ];
 
                     neighbours
                         .iter()
                         .max_by(|a, b| {
-                            if (a.score - b.score).abs() < f32::EPSILON {
-                                Ordering::Equal
-                            } else if a.score > b.score {
+                            if a.score > b.score {
                                 Ordering::Greater
                             } else {
                                 Ordering::Less
@@ -156,15 +161,70 @@ fn calculate_score(patch: &Patch, neighbours: &Vec<&Patch>, alpha: f32) -> f32 {
     }
 }
 
-fn print_playing_field(playing_field: &PlayingField) {
-    print!("\x1B[2J\x1B[1;1H");
+fn print_playing_field(playing_field: &PlayingField) -> std::io::Result<()> {
+    use std::io::Write;
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+    write!(lock,"\x1B[2J\x1B[1;1H")?;
     for patch_row in &playing_field.patches {
         for patch in patch_row {
             match patch.strategy {
-                Strategy::Helping => print!("✅"),
-                Strategy::Leeching  => print!("❌")
-            }
+                Strategy::Helping => write!(lock, "🟢")?,
+                Strategy::Leeching  => write!(lock, "🟡")?
+            };
         }
-        println!();
+
+        // write!(lock, "  ")?;
+        // for patch in patch_row {
+        //     write!(lock, "{},", patch.score)?;
+        // }
+        writeln!(lock)?;
+    }
+    writeln!(lock)?;
+
+    Ok(())
+}
+
+#[test]
+fn single_leeching_patch() {
+    let alpha = 0_f32;
+    let patches = vec![vec![Patch::new(Strategy::Leeching); 1]; 1];
+    let mut playing_field = PlayingField::with_patches(patches);
+
+    assert_eq!(playing_field.patches[0][0].strategy, Strategy::Leeching);
+
+    for _ in 0..10 {
+        playing_field.step(alpha);
+        assert_eq!(playing_field.patches[0][0].strategy, Strategy::Leeching);
+    }
+}
+
+#[test]
+fn single_helping_patch() {
+    let alpha = 0_f32;
+    let patches = vec![vec![Patch::new(Strategy::Helping); 1]; 1];
+    let mut playing_field = PlayingField::with_patches(patches);
+
+    assert_eq!(playing_field.patches[0][0].strategy, Strategy::Helping);
+
+    for _ in 0..10 {
+        playing_field.step(alpha);
+        assert_eq!(playing_field.patches[0][0].strategy, Strategy::Helping);
+    }
+}
+
+#[test]
+fn single_step_small_grid() {
+    let alpha = 0_f32;
+    let patches = vec![vec![Patch::new(Strategy::Helping); 3]; 3];
+    let mut playing_field = PlayingField::with_patches(patches.to_owned());
+
+    playing_field.step(alpha);
+
+    for patch_row in playing_field.patches {
+        for patch in patch_row {
+            assert_eq!(patch.strategy, Strategy::Helping);
+            assert_eq!(patch.score, 8_f32);
+        }
     }
 }
